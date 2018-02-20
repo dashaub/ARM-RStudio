5228071e9d1d625bfc8bf97b1b58e456c1f585f9
David Shaub <davidshaub@gmx.com>
2018-02-20 12:56:30 -0700

    big refactor

--- ARM-RStudio.sh
+++ ARM-RStudio.sh
@@ -1,54 +1,29 @@
 #!/bin/bash
-#This script installs R and builds RStudio Desktop for ARM Chromebooks running Ubuntu 16.04
+# This script installs R and builds RStudio Desktop for ARM Chromebooks running Ubuntu 14.04/16.04
+set -e
 
-#Install R
+# Install R
 sudo apt-get update
-sudo apt-get install -y r-base r-base-dev
+sudo apt-get install -y r-base r-base-dev wget
 
-#Download RStudio source
-#Set RStudio version
+# Download RStudio source
+# Set RStudio version
 VERS=v1.1.423
 cd
 wget https://github.com/rstudio/rstudio/tarball/$VERS
 mkdir rstudio-$VERS && tar xf $VERS -C rstudio-$VERS --strip-components 1
 rm $VERS
 
-#Install RStudio build dependencies
-sudo apt-get install -y git
-sudo apt-get install -y build-essential pkg-config fakeroot cmake ant libjpeg62
-sudo apt-get install -y uuid-dev libssl-dev libbz2-dev zlib1g-dev libpam-dev
-sudo apt-get install -y libapparmor1 apparmor-utils libboost-all-dev libpango1.0-dev
-sudo apt-get install -y openjdk-8-jdk
-sudo apt-get install -y cabal-install
-sudo apt-get install -y ghc
-sudo apt-get install -y qt-sdk
-sudo apt-get install -y pandoc
+# Install dependencies
+sudo rstudio-$VERS/dependencies/linux/install-dependencies-debian
 
-#Run common environment preparation scripts
-cd rstudio-$VERS/dependencies/common/
-mkdir ~/rstudio-$VERS/dependencies/common/pandoc
-cd ~/rstudio-$VERS/dependencies/common/
-./install-gwt
-./install-dictionaries
-./install-mathjax
-./install-boost
-./install-packages
-
-#Get Closure Compiler and replace compiler.jar
-cd
-wget http://dl.google.com/closure-compiler/compiler-latest.zip
-unzip compiler-latest.zip
-rm COPYING README.md compiler-latest.zip
-sudo mv closure-compiler*.jar ~/rstudio-$VERS/src/gwt/tools/compiler/compiler.jar
-
-#Configure cmake and build RStudio
+# Configure cmake and build RStudio
 cd ~/rstudio-$VERS/
 mkdir build
-sudo cmake -DRSTUDIO_TARGET=Desktop -DCMAKE_BUILD_TYPE=Release
+cd build
+sudo cmake .. -DRSTUDIO_TARGET=Desktop -DCMAKE_BUILD_TYPE=Release
 sudo make install
 
-#Clean the system of packages used for building
-cd
-sudo apt-get autoremove -y cabal-install ghc pandoc libboost-all-dev
-sudo rm -rf rstudio-$VERS
-sudo apt-get autoremove -y
+# Clean the system of packages used for building
+cd ~
+rm -rf rstudio-$VERS
