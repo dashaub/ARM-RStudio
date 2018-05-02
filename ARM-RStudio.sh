7f63cd8861483a0f0b775e2bee577afc7ba5a2f3
David Shaub <dashaub@users.noreply.github.com>
2018-05-02 13:10:37 -0600

    prevent expansion

--- ARM-RStudio.sh
+++ ARM-RStudio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script installs R and builds RStudio Desktop for ARM Chromebooks running Ubuntu 16.04
-set -e
+set -euo pipefail
 
 # Install R
 sudo apt-get update
@@ -8,24 +8,24 @@ sudo apt-get install -y r-base r-base-dev wget openjdk-8-jdk checkinstall
 
 # Download RStudio source
 # Set RStudio version
-VERS=v1.1.423
-cd ~
-wget https://github.com/rstudio/rstudio/tarball/$VERS
-mkdir rstudio-$VERS && tar xf $VERS -C rstudio-$VERS --strip-components 1
-rm $VERS
+VERS='v1.1.447'
+cd /tmp
+wget "https://github.com/rstudio/rstudio/tarball/${VERS}"
+mkdir "rstudio-$VERS" && tar xf "${VERS}" -C "rstudio-${VERS}" --strip-components 1
+rm "${VERS}"
 
 # Install dependencies
 cd rstudio-$VERS/dependencies/linux
 sudo ./install-dependencies-debian
 
 # Configure cmake and build RStudio
-cd ~/rstudio-$VERS/
+cd "/tmp/rstudio-$VERS/"
 mkdir build
 cd build
 sudo cmake .. -DRSTUDIO_TARGET=Desktop -DCMAKE_BUILD_TYPE=Release
-PKG_VERS=$(echo $VERS | sed 's/^v//')
-sudo checkinstall --pkgname rstudio --pkgversion $PKG_VERS -y
+PKG_VERS=$(echo "${VERS}" | sed 's/^v//')
+sudo checkinstall --pkgname rstudio --pkgversion "${PKG_VERS}" -y
 
 # Clean the system of packages used for building
-cd ~
-rm -rf rstudio-$VERS
+cd /tmp
+rm -rf "rstudio-$VERS"
