ac6daaa07b6ffbad1314970d915ff2c792349030
David Shaub <david.shaub@logrhythm.com>
2019-01-15 14:19:08 -0700

    older rstudio version

--- ARM-RStudio.sh
+++ ARM-RStudio.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-#This script installs R and builds RStudio Desktop for ARM Chromebooks running Ubuntu 16.04
+#This script installs R and builds RStudio Desktop for ARM Chromebooks running Ubuntu 14.04
 
 #Install R
 sudo apt-get update
-sudo apt-get install -y r-base r-base-dev
+sudo apt-get -qq install -y r-base r-base-dev
 
 #Download RStudio source
 #Set RStudio version
-VERS=v1.1.423
+VERS=v0.98.982
 cd
 wget https://github.com/rstudio/rstudio/tarball/$VERS
 mkdir rstudio-$VERS && tar xf $VERS -C rstudio-$VERS --strip-components 1
 rm $VERS
 
 #Install RStudio build dependencies
-sudo apt-get install -y git
-sudo apt-get install -y build-essential pkg-config fakeroot cmake ant libjpeg62
-sudo apt-get install -y uuid-dev libssl-dev libbz2-dev zlib1g-dev libpam-dev
-sudo apt-get install -y libapparmor1 apparmor-utils libboost-all-dev libpango1.0-dev
-sudo apt-get install -y openjdk-8-jdk
-sudo apt-get install -y cabal-install
-sudo apt-get install -y ghc
-sudo apt-get install -y qt-sdk
-sudo apt-get install -y pandoc
+sudo apt-get install -qq -y git
+sudo apt-get install -qq -y build-essential pkg-config fakeroot cmake ant libjpeg62
+sudo apt-get install -qq -y uuid-dev libssl-dev libbz2-dev zlib1g-dev libpam-dev
+sudo apt-get install -qq -y libapparmor1 apparmor-utils libboost-all-dev libpango1.0-dev
+sudo apt-get install -qq -y openjdk-7-jdk
+sudo apt-get install -qq -y cabal-install
+sudo apt-get install -qq -y ghc
+sudo apt-get install -qq -y qt-sdk
+sudo apt-get install -qq -y pandoc
 
 #Run common environment preparation scripts
 cd rstudio-$VERS/dependencies/common/
@@ -31,7 +31,7 @@ cd ~/rstudio-$VERS/dependencies/common/
 ./install-gwt
 ./install-dictionaries
 ./install-mathjax
-./install-boost
+./install-boost > boostlog.log
 ./install-packages
 
 #Get Closure Compiler and replace compiler.jar
@@ -50,5 +50,5 @@ sudo make install
 #Clean the system of packages used for building
 cd
 sudo apt-get autoremove -y cabal-install ghc pandoc libboost-all-dev
-sudo rm -rf rstudio-$VERS
+sudo rm -r -f rstudio-$VERS
 sudo apt-get autoremove -y
